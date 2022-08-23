4eb8d960832fdda2c7c0dfc1b1d18d3ec4d8c4cb
Matt <matt.titmarsh@gmail.com>
2022-08-23 19:21:02 +1000

    Add edit, error messages and success messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :password, presence: true
-  validates :email, presence: true
+
+  validates :username, presence: true, uniqueness: true, length:{ minimum: 3, maximum: 20 }
+  validates :email, presence: true, uniqueness: true, length:{ minimum: 3, maximum: 100 }
+  validates :password, presence: true, length:{ minimum: 3, maximum: 20 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  root 'users#new'
+  resources :users, only: [:new, :create, :edit, :update]
 end
